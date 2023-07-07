509cac0b6742566db3b3e76756bad530e569627c
gautam <gautamjuneja03@gmail.com>
2023-07-07 18:59:54 +0530

    Folder within folder done

--- ShareHub/settings.py
+++ ShareHub/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'FiShare.urls'
+ROOT_URLCONF = 'ShareHub.urls'
 
 TEMPLATES = [
     {
@@ -73,7 +73,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'FiShare.wsgi.application'
+WSGI_APPLICATION = 'ShareHub.wsgi.application'
 
 
 # Database
--- users/views.py
+++ users/views.py
@@ -50,11 +50,16 @@ def profile(request):
         u_form = UserUpdateForm(request.POST,instance = request.user)
         p_form = ProfileUpdateForm(request.POST,request.FILES,instance = request.user.profile)
 
-        if u_form.is_valid() and p_form.is_valid():
+        if u_form.is_valid():
             u_form.save()
-            p_form.save()
+            
             messages.success(request,f'Your profile has been updated!')
             return redirect('profile')
+        if p_form.is_valid():
+            p_form.save()
+            messages.success(request,f'Your profile picture has been updated!')
+            return redirect('profile')
+
     else:
         u_form = UserUpdateForm(instance = request.user)
         p_form = ProfileUpdateForm(instance = request.user.profile)
